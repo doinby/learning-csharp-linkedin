45b3b08433f412a8eca485e98ea54ab38d9143d6
Chinh Do <chinh@doinby.co>
2023-06-15 17:32:30 +0700

    Conditionals with "switch"

---
+++ Conditional-if/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theVal = 50;
+
+            // TODO: if-else
+            // if (theVal == 50)
+            // {
+            //     Console.WriteLine("theVal is 50");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("theVal is something else");
+            // }
+
+
+            // -----------------------
+            // TODO: Using the ternary operator ?:
+            Console.WriteLine(theVal == 50 ? "theVal is 50" : "theVal is something else");
+
+            // a two-case if-then
+            /*
+            if (theVal < 50) {
+                Console.WriteLine("theVal is small");
+            }
+            else {
+                Console.WriteLine("theVal is large");
+            }
+            */
+
+            // TODO: can be replaced by a ternary operator ?:
+
+        }
+    }
+}
