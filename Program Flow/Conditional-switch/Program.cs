c23409d8962915847405fd328f3456def0a355c4
Chinh Do <chinh@doinby.co>
2023-06-16 12:36:41 +0700

    Reorder file structure

---
+++ Program Flow/Conditional-switch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConditionalOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theVal = 55;
+
+            // TODO: The switch statement
+            switch (theVal)
+            {
+                case 50:
+                    Console.WriteLine("theVal is 50");
+                    break;
+                default:
+                    Console.WriteLine("theVal is something else");
+                    break;
+            }
+
+        }
+    }
+}
